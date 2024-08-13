1ad8f678a790646483278dc47cfe3853e9f05ba6
Vishaal Kumar <59760349+VishaalPKumar@users.noreply.github.com>
2024-08-13 18:08:47 +0400

    Initial Commit

---
+++ Rummy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rummy
+//
+//  Created by Vishaal Kumar on 8/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rummy/RummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RummyApp.swift
+//  Rummy
+//
+//  Created by Vishaal Kumar on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct RummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
